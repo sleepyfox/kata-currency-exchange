767ea1dc1d57f7574fc1bcc98baed6cf2de61af9
@sleepyfox <sleepyfox@gmail.com>
2021-12-12 16:51:14 +0000

    refactor: improve test names

--- spec/currency_converter_spec.js
+++ spec/currency_converter_spec.js
@@ -17,31 +17,31 @@ describe('A currency converter API', () => {
      expect(convert(1, 'USD', 'EUR')).toBe(0.87815)
     })
 
-    it('should error if non-USD base currency', () => {
-      expect(() => convert(1, 'EUR', 'USD')).toThrow()
+    it('should convert 1 USD to 0.87815 EUR', () => {
+     expect(convert(1, 'USD', 'EUR')).toBe(0.87815)
     })
   })
 
   describe('A USD conversion rate lookup', () => {
-    it('should convert USD to USD at 1.0', () => {
+    it('should show rate of USD to USD at 1.0', () => {
       expect(USDConversionRate('USD')).toBe(1.0)
     })
 
-    it('should convert USD to EUR at 0.87815', () => {
+    it('should show rate of USD to EUR at 0.87815', () => {
       expect(USDConversionRate('EUR')).toBe(0.87815)
     })
 
-    it('should convert USD to GBP at 0.78569', () => {
+    it('should show rate of USD to GBP at 0.78569', () => {
       expect(USDConversionRate('GBP')).toBe(0.78569)
     })
   })
 
   describe('A to USD conversion rate lookup', () => {
-    it('should convert USD to USD at 1.0', () => {
+    it('should show rate of USD to USD at 1.0', () => {
       expect(ToUSDConversionRate('USD')).toBe(1.0)
     })
 
-    it('should convert EUR to USD at 1.13876', () => {
+    it('should show rate of EUR to USD at 1.13876', () => {
       expect(ToUSDConversionRate('EUR')).toBeCloseTo(1.13876, 5)
     })
   })
